2ece5c2b3ba737ec9dfe06347cde2410048a7263
Tomgxz <thomaslee10.TL@gmail.com>
2023-08-11 13:20:29 +0100

    Initial Commit

---
+++ __init__.py
@@ -0,0 +1,113 @@
+from dmx import DMX
+import numpy as np
+import math,time
+from mss import mss
+from PIL import Image,ImageEnhance
+from serial import SerialException
+
+class DMXControl():
+    
+    def __init__(self):
+        self.FORMAT = [0,1,2]
+        self.PIXELCOUNT = 16
+        self.DMXVALUES = len(self.FORMAT)*self.PIXELCOUNT
+        self.FADEAMT = 15
+
+        self.BLACK = np.array([[0 for __ in range(len(self.FORMAT))] for _ in range(self.PIXELCOUNT)])
+
+        self.DIMMED = 1
+        self.MONO = False
+
+        self.previouscontent = self.BLACK
+
+        self.connected = False
+
+        while True:
+
+            try:
+
+                if not self.connected:
+
+                    try: 
+                        self.connect()
+                        self.connected = True
+
+                        print("RS-485 DMX Interface connected")
+
+                    except Exception as e:
+
+                        print("Could not connect to RS-485 DMX Interface, retrying in 5 seconds... ")
+                        
+                        time.sleep(5)
+                        
+                        continue
+
+                self.update()
+        
+            except SerialException as e:
+            
+                print("RS-485 DMX Interface disconnected, attempting to reconnect...")
+                self.connected = False
+
+    def connect(self):
+        self.interface = DMX(num_of_channels=self.DMXVALUES)
+        self.screencapture = mss()
+
+    def update(self):
+        #print("UPDATE")
+
+        sct_img = self.screencapture.grab({"top":270,"left":480,"width":960,"height":540})
+
+        img = Image.frombytes("RGB",(sct_img.width,sct_img.height),sct_img.rgb)
+        img = ImageEnhance.Color(img).enhance(2)
+        img = img.resize((1 if self.MONO else 16,1),Image.ANTIALIAS)
+
+        img = np.array(img)[0]
+
+        i=1
+
+        if not self.MONO: # if mono is false
+            for pixel in img: # for pixel in image (where the image is 1 pixel high and a np.array)
+                for sub in pixel: # for part (of [r,g,b]) in pixel
+
+                    # get the previous content of this pixel
+                    prev = self.previouscontent[math.floor((i-1)/3)][(i-1)%3]
+
+                    # if the previous content was a lower value
+                    if int(prev)-int(sub) <= -self.FADEAMT: 
+                        sub = prev + self.FADEAMT
+
+                    # if the previous content was a higher value
+                    if int(prev)-int(sub) >= self.FADEAMT: 
+                        sub = prev - self.FADEAMT
+
+                    # clamp the new value between 0 and 255
+                    sub = max(min(sub, 255),0)
+
+                    # format the dmx value
+                    msub = int(self.colorfunction(sub)*(0.6 if (i-1)%3 == 2 else 1))
+
+                    # set the value of the pixel in question, but dont send to the interface
+                    self.interface.set_data(i,msub,auto_send=False)
+
+                    # set the value of the pixel in the image array so that it can be stored for self.previouscontent
+                    img[math.floor((i-1)/3)][(i-1)%3] = sub
+                    i+=1
+
+                    if i > self.DMXVALUES: break
+                if i > self.DMXVALUES: break
+
+        else: # if the image is mono
+
+            pass
+
+        # store in previous content for fade functions
+        self.previouscontent = img
+
+        # send the dmx content through the interface
+        self.interface.send()
+    
+    def colorfunction(self,x:int) -> int:
+        return ((x/510)**2)*1020*self.DIMMED
+
+DMXControl()
