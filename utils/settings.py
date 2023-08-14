23085e22d3aca7c2ce2fddc97c9c9167e234e604
Tomgxz <thomaslee10.TL@gmail.com>
2023-08-14 14:30:35 +0100

    Added more settings
    Added PIXEL_REDUCE, currently unused
    Added COLOR_ENHANCE
    Updated comment for COLOR_MONO, now deprecated due to PIXEL_REDUCE

--- utils/settings.py
+++ utils/settings.py
@@ -5,6 +5,12 @@ if __name__ == "__main__":
 # The amount of pixels to output to
 PIXEL_COUNT = 16
 
+# How much to reduce the output. Must be a factor of PIXEL_COUNT
+# 1 means that each pixel will have a unique output
+# 2 means that each pair of pixels will be the same
+# if PIXEL_REDUCE == PIXEL_COUNT, only one color will be outputted to all pixels
+PIXEL_REDUCE = 1
+
 # The factor with which fade time is calculated
 FADE_FACTOR = 15
 
@@ -14,8 +20,12 @@ DIMMED = 1
 # The DMX address format for each pixel
 COLOR_FORMAT = ["R","G","B"]
 
-# Whether or not the output is black and white
+# Whether or not the output all one color, or many colors
+# Deprecated, replaced by PIXEL_REDUCE
 COLOR_MONO = 0
+
+# How much to enhance the image, 1 preserves its original state
+COLOR_ENHANCE = 2
 
 # The time (in ms) between each attempted connection to the interface
 INTERFACE_CONNECTION_DELAY = 5000
